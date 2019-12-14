abe1c8c1abb67f16a06407a713c26c90464591e0
Piyush.Thakre <Piyush.Thakre@ISIPL-PUNV-L092.corp.intelliswift.com>
2019-12-14 11:51:52 +0530

    Genric constant

---
+++ ConstructionMaterialManagement/src/main/java/com/app/utility/GenericConstant.java
@@ -0,0 +1,7 @@
+package com.app.utility;
+
+public class GenericConstant {
+	public static final String SUCCESS = "Success";
+	public static final String FAIL="fail";
+	public static final String SERVERERROR="Internal Server Error";
+}
