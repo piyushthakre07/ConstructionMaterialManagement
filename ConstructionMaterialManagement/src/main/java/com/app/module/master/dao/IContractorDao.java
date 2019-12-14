3c12faf2db0647ef2d7dbbdf440bc729045c8718
Piyush.Thakre <Piyush.Thakre@ISIPL-PUNV-L092.corp.intelliswift.com>
2019-12-14 22:55:49 +0530

    master changes

--- ConstructionMaterialManagement/src/main/java/com/app/module/master/dao/IContractorDao.java
+++ ConstructionMaterialManagement/src/main/java/com/app/module/master/dao/IContractorDao.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.app.model.Contractor;
-import com.app.model.Vendor;
 @Repository
 public interface IContractorDao extends JpaRepository<Contractor, Long> {	
 }
